5978ec04422320b61fc20b16b872979570a202bc
unknown <brannongarrett77@gmail.com>
2024-01-09 19:24:17 +0100

    Added frontend updates for the search box

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
 ]
 
 
