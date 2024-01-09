d91aa84dae17ec952f87dd1e13eeecf302a99327
unknown <brannongarrett77@gmail.com>
2024-01-09 18:10:57 +0100

    Added logic to the view.py page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 from . import util
 
-
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
-
+def index(request, title):
+    # If title is empty, return an error page
+    entry = util.get_entry(title)
+    if entry is None:
+        return render(request, "encyclopedia/error.html", {
+            "error": "Page not found"
+        })
+    else:
+        # If title is not empty, return the entry page
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": entry
+        })
